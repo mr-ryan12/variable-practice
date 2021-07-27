d3afe37764f32ffea57fc350d91a17a286b43d85
Ryan McBride <mr.ryan444@gmail.com>
2021-07-26 20:48:32 -0600

    Add dog variable as object

--- model-1.js
+++ model-1.js
@@ -2,9 +2,29 @@ var name = 'Cat Stevens';
 var color = 'Brown';
 var size = 'Large';
 var age = 2;
-var numOfToys = 10;
+var numOfToys = 3;
 var howManyLives = 1;
 var isFluffy = true;
 var isEatingMyCouch = true;
 var isHungry = false;
 var toys = ['squeeky fish', 'a big rope', 'sticks n stuff'];
+
+var dog = {
+    name: 'Cat Stevens',
+    color: 'Brown',
+    size: 'Large',
+    breed: 'Airedale Terrier',
+    age: 2,
+    numOfToys: 3,
+    howManyLives: 1,
+    isFluffy: true,
+    isEatingMyCouch: true,
+    isHungry: false,
+    toys: ['squeeky fish', 'a big rope', 'sticks n stuff'],
+
+    bark() {
+        console.log('Bark!!');
+    }
+}
+
+dog.bark();
